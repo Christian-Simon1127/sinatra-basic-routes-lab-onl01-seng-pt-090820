0560c31f801ff84bb519bdb516f90d0fa335bbcc
Christian Simon <csimon1127@gmail.com>
2020-12-09 20:51:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,16 +6,16 @@ class App < Sinatra::Base
     "Hello, World!"
   end 
   
-  get '/home' do 
-    
+  get '/hometown' do 
+    "My hometown is Rome, GA!"
   end
   
   get '/name' do 
-    
+    "My name is Christian!"
   end
   
   get '/favorite-song' do
-  
+    "My favorite song is non-existent, but I love the 80's."
   end
   
   
